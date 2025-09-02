8e026f52e647459a0b57e4eaee3d728beb5cf949
Natha-daa <nathanaeldaawe@gmail.com>
2025-09-02 12:59:12 +0200

    Update models.py

--- models.py
+++ models.py
@@ -1,12 +1,17 @@
-from sqlalchemy import Column, Integer, String
-from db import Base
+# app/models.py
+from sqlalchemy import Column, Integer, String, Text, DateTime
+from sqlalchemy.sql import func
+from app.db import Base
 
 class Media(Base):
-    __tablename__ = "media"
+    __tablename__ = "Media"
 
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, nullable=False)
-    description = Column(String, nullable=True)
-    number_speaker = Column(Integer, nullable=True)
-    audio_path = Column(String, nullable=True)
-    video_path = Column(String, nullable=True)
+    name = Column(Text, nullable=False)
+    description = Column(Text, nullable=True)
+    createdAt = Column(DateTime(timezone=True), server_default=func.now(), nullable=False)
+    audioLink = Column(Text, nullable=True)
+    videoLink = Column(Text, nullable=True)
+    coverLink = Column(Text, nullable=True)
+    fileSize = Column(String, nullable=True)
+    numberSpeaker = Column(Integer, nullable=False, default=1)
