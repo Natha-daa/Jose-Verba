98d21509edb4226b0a603b18cc762f75fdd79824
Natha-daa <nathanaeldaawe@gmail.com>
2025-09-02 16:11:14 +0200

    Update main.py

--- main.py
+++ main.py
@@ -33,15 +33,14 @@ from langchain_tavily import TavilySearch
 from sqlalchemy.orm import Session
 import models
 import schemas
-from db import Base
-
+from db import Base, engine, get_db
+import uvicorn
 
 # -----------------------------
 # CONFIG
 # -----------------------------
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-DB_FILE = "speakers_store/spearker_voice.db"
-
+# DB_FILE = "speakers_store/spearker_voice.db"  # Supprimé, remplacé par PostgreSQL
 
 load_dotenv('.env.local')
 HF_TOKEN = os.getenv("HF_TOKEN")
@@ -61,6 +60,25 @@ pipeline = Pipeline.from_pretrained(
 )
 
 llm = whisper.load_model("turbo")  # ou "small", "medium", "large"
+
+# Lazy loading des modèles pour optimiser la RAM au démarrage
+def load_llm():
+    return whisper.load_model("turbo")  # ou "small", "medium", "large"
+
+def load_classifier():
+    return EncoderClassifier.from_hparams(
+        source="speechbrain/spkrec-ecapa-voxceleb",
+        run_opts={"device": DEVICE}
+    ).to(DEVICE)
+
+def load_pipeline():
+    return Pipeline.from_pretrained(
+        "pyannote/speaker-diarization-3.1",
+        use_auth_token=HF_TOKEN
+    )
+
+def load_model_gemini():
+    return ChatGoogleGenerativeAI(model="gemini-2.0-flash-lite", google_api_key="AIzaSyAv5QViz6g3qDDiaX-jI3bzZ2HEf-7rXL0")
 
 # Crée les tables si elles n'existent pas
 Base.metadata.create_all(bind=engine)
@@ -94,26 +112,26 @@ async def create_media(
     video: UploadFile = None,
     db: Session = Depends(get_db)
 ):
-    os.makedirs("uploads", exist_ok=True)
+    os.makedirs("/app/uploads", exist_ok=True)
 
     audio_link, video_link, file_size = None, None, 0
 
     # Sauvegarde audio
     if audio:
         audio_filename = f"{uuid.uuid4()}_{audio.filename}"
-        audio_path = os.path.join("uploads", audio_filename)
+        audio_path = os.path.join("/app/uploads", audio_filename)
         with open(audio_path, "wb") as f:
             f.write(await audio.read())
-        audio_link = f"/uploads/{audio_filename}"
+        audio_link = f"/file/{audio_filename}"
         file_size = audio.size or 0
 
     # Sauvegarde vidéo
     if video:
         video_filename = f"{uuid.uuid4()}_{video.filename}"
-        video_path = os.path.join("uploads", video_filename)
+        video_path = os.path.join("/app/uploads", video_filename)
         with open(video_path, "wb") as f:
             f.write(await video.read())
-        video_link = f"/uploads/{video_filename}"
+        video_link = f"/file/{video_filename}"
         file_size = video.size or 0
 
     # Création de l'objet Media et insertion dans PostgreSQL
@@ -134,7 +152,7 @@ async def create_media(
 # === ROUTE pour récupérer un fichier uploadé ===
 @app.get("/file/{file_name}")
 def get_file(file_name: str):
-    file_path = os.path.join("uploads", file_name)
+    file_path = os.path.join("/app/uploads", file_name)
     if not os.path.exists(file_path):
         raise HTTPException(status_code=404, detail="File not found")
     return FileResponse(path=file_path)
@@ -144,6 +162,7 @@ def get_file(file_name: str):
 def get_all_media(db: Session = Depends(get_db)):
     medias = db.query(models.Media).all()
     return medias    
+
 class TranscriptionResponse(BaseModel):
     text: str
 
@@ -265,13 +284,13 @@ def parse_single_psych_profile(output_text: str, speaker: str) -> PsychProfile:
 @app.post("/transcription/internal")
 async def transcription(audio_id: str = Query(..., description="identifiant de l'audio hébergée")):
     try:
-        audio_data = os.path.join("uploads", audio_id)
+        audio_data = os.path.join("/app/uploads", audio_id)
         if not os.path.exists(audio_data):
             raise HTTPException(status_code=404, detail="File not found")
         else:
             print("audio found")
             # Transcription locale avec Whisper
-            result = llm.transcribe(audio_data)
+            result = load_llm().transcribe(audio_data)
             return {"text": result["text"]}
 
     except Exception as e:
@@ -280,15 +299,18 @@ async def transcription(audio_id: str = Query(..., description="identifiant de l
 @app.post("/diarisation/internal")
 async def diarisation(audio_id: str = Query(..., description="identifiant de l'audio hébergée"), num_speakers: int = Query(..., description="Nombre de locuteurs")):
     try:
-        audio_data = os.path.join("uploads", audio_id)
+        audio_data = os.path.join("/app/uploads", audio_id)
         if not os.path.exists(audio_data):
             raise HTTPException(status_code=404, detail="File not found")
         else:
             print("audio found")
 
             # Diarisation locale avec Whisper
-            seg = extract_speakers(llm, classifier, audio_data,num_speakers)
-            result = write_segments(seg, 'temp/transcript.txt')
+            llm = load_llm()
+            classifier = load_classifier()
+            pipeline = load_pipeline()
+            seg = extract_speakers(llm, classifier, audio_data, num_speakers)
+            result = write_segments(seg, '/app/temp/transcript.txt')
             return JSONResponse(content=result)
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
@@ -300,7 +322,7 @@ async def create_upload_file(file: Union[UploadFile, None] = None):
             raise HTTPException(status_code=400, detail="No upload file sent")
         else:
             filename=str(uuid.uuid4())+"_"+file.filename
-            file_path=os.path.join("uploads", filename)
+            file_path=os.path.join("/app/uploads", filename)
             print(file_path)
             with open(file_path, "wb") as f:
                 f.write(file.file.read())
@@ -308,24 +330,22 @@ async def create_upload_file(file: Union[UploadFile, None] = None):
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
-
 @app.get("/file/{name_file}")
 def get_file(name_file: str):
     try:
-        if not os.path.exists(os.path.join("uploads", name_file)):
+        if not os.path.exists(os.path.join("/app/uploads", name_file)):
             raise HTTPException(status_code=404, detail="File not found")
         else:
-            return FileResponse(path=os.path.join("uploads", name_file))
+            return FileResponse(path=os.path.join("/app/uploads", name_file))
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
-
 
 @app.post("/fact-checking")
 async def fact_check_agent(transcript: Transcript):
     results = []
     for seg in transcript.transcript:
         prompt = generate_fact_extraction_prompt(seg.text, seg.speaker, seg.start_time)
-        result = model_gemini.invoke(prompt)
+        result = load_model_gemini().invoke(prompt)
         parse_result = clean_json_string(result.content)
         parse_result: List[Dict] = json.loads(parse_result)
         tavily_search_result = ""
@@ -345,14 +365,13 @@ async def fact_check_agent(transcript: Transcript):
             
         fact_check_prompt = generate_fact_check_prompt(fact["statement"], seg.speaker, seg.start_time, tavily_search_result, wikipedia_search_result, serper_video_search_result)
         print(fact_check_prompt)
-        result = model_gemini.invoke(fact_check_prompt)
+        result = load_model_gemini().invoke(fact_check_prompt)
         print(result.content)
         parse_result = clean_json_string(result.content)
         parse_result = json.loads(parse_result)
         results.append(parse_result)
 
     return results
-
 
 @app.post("/ideology", response_model=List[IdeologyResult])
 async def ideology(transcript: Transcript):
@@ -407,7 +426,7 @@ async def ideology(transcript: Transcript):
     {synthese}
     """)
 
-    chain = prompt | model_gemini
+    chain = prompt | load_model_gemini()
 
     result = chain.invoke({"synthese": speakers_formatted})
 
@@ -437,7 +456,6 @@ async def ideology(transcript: Transcript):
 
 @app.post("/psych-profile", response_model=List[PsychProfile])
 async def analyze_psych_profile(transcript: Transcript):
-
     # 🧠 2️⃣ Profilage psychologique
     speakers = {}
     for seg in transcript.transcript:
@@ -519,13 +537,12 @@ async def analyze_psych_profile(transcript: Transcript):
         - Sois précis, concis et rigoureux.
 
         """
-        result = model_gemini.invoke(prompt)
+        result = load_model_gemini().invoke(prompt)
 
         profile = parse_single_psych_profile(result.content,speaker)
         results.append(profile)
 
     return results
-
 
 @app.post("/summarize", response_model=SummaryResult)
 def summarize(transcript: Transcript):
@@ -568,7 +585,7 @@ def summarize(transcript: Transcript):
 
     # 3️⃣ Génère
     print(prompt)
-    chain = prompt | model_gemini
+    chain = prompt | load_model_gemini()
 
     result = chain.invoke({"dialogue": all_text})
     print(result.content)
@@ -591,9 +608,9 @@ def summarize(transcript: Transcript):
     return SummaryResult(global_summary=global_summary, by_speaker=by_speaker)
 
 @app.post("/add-speaker-voice", response_model=Dict[str, Union[str, int]])
-async def add_speaker(speaker_name: str = Form(...), file: UploadFile = None):
+async def add_speaker(speaker_name: str = Form(...), file: UploadFile = None, db: Session = Depends(get_db)):
     # Sauver temporairement le fichier audio
-    tmp_path = f"/tmp/voice/{speaker_name}.wav"
+    tmp_path = f"/app/temp/voice/{speaker_name}.wav"
     
     if not os.path.exists(tmp_path):
         os.makedirs(os.path.dirname(tmp_path), exist_ok=True)
@@ -602,38 +619,36 @@ async def add_speaker(speaker_name: str = Form(...), file: UploadFile = None):
         f.write(await file.read())
     
     # Extraire l'embedding
-    embedding = extract_speaker_embeddings(tmp_path, DEVICE,classifier)
+    embedding = extract_speaker_embeddings(tmp_path, DEVICE, load_classifier())
     embedding_json = json.dumps(embedding.tolist())
 
-    with sqlite3.connect(DB_FILE) as conn:
-        conn.execute(
-            "INSERT INTO speakers (speaker_name, embedding) VALUES (?, ?)",
-            (speaker_name, embedding_json)
-        )
+    # Ajout dans PostgreSQL
+    speaker = models.Speaker(speaker_name=speaker_name, embedding=embedding_json)
+    db.add(speaker)
+    db.commit()
+    db.refresh(speaker)
 
     return {"message": f"Speaker '{speaker_name}' ajouté avec succès.", "embedding_dim": len(embedding)}
 
 @app.get("/all-speakers-voice")
-async def list_speakers():
-    with sqlite3.connect(DB_FILE) as conn:
-        cur = conn.cursor()
-        cur.execute("SELECT id, speaker_name FROM speakers")
-        rows = cur.fetchall()
-    
-    return [{"id": row[0], "speaker_name": row[1]} for row in rows]
+async def list_speakers(db: Session = Depends(get_db)):
+    speakers = db.query(models.Speaker).all()
+    return [{"id": speaker.id, "speaker_name": speaker.speaker_name} for speaker in speakers]
 
 @app.post("/identify-speaker", response_model=Dict[str, Union[str, float]])
-async def identify_speaker_sqlite(file: UploadFile):
-    tmp_path = f"/tmp/{file.filename}.wav"
+async def identify_speaker_sqlite(file: UploadFile, db: Session = Depends(get_db)):
+    tmp_path = f"/app/temp/{file.filename}.wav"
     if not os.path.exists(tmp_path):
         os.makedirs(os.path.dirname(tmp_path), exist_ok=True)
     
     with open(tmp_path, "wb") as f:
         f.write(await file.read())  
     
-    new_emb = extract_speaker_embeddings(tmp_path, DEVICE,classifier)
+    new_emb = extract_speaker_embeddings(tmp_path, DEVICE, load_classifier())
     
-    best_match = find_nearest_speaker(new_emb, DB_FILE)
+    speakers = db.query(models.Speaker).all()
+    known_speakers = [(speaker.speaker_name, np.array(json.loads(speaker.embedding))) for speaker in speakers]
+    best_match = find_nearest_speaker(new_emb, known_speakers)
 
     if best_match:
         return {"speaker": best_match["speaker"], "similarity": best_match["similarity"]}
@@ -641,17 +656,16 @@ async def identify_speaker_sqlite(file: UploadFile):
         return {"speaker": None, "similarity": 0.0}
 
 @app.post("/asr")
-async def asr(file: UploadFile):
-
-    tmp_path = f"/tmp/{file.filename}.wav"
+async def asr(file: UploadFile, db: Session = Depends(get_db)):
+    tmp_path = f"/app/temp/{file.filename}.wav"
     if not os.path.exists(tmp_path):
         os.makedirs(os.path.dirname(tmp_path), exist_ok=True)
     
     with open(tmp_path, "wb") as f:
         f.write(await file.read()) 
     
-    transcript = llm.transcribe(tmp_path)
-    segments = pipeline(tmp_path)
+    transcript = load_llm().transcribe(tmp_path)
+    segments = load_pipeline()(tmp_path)
     
     # Set a threshold for similarity scores to determine when a match is considered successful
     threshold = 0.7
@@ -668,19 +682,15 @@ async def asr(file: UploadFile):
         waveform = waveform.to(DEVICE)
 
         # Extract the speaker embedding from the audio segment
-        embedding = classifier.encode_batch(waveform).squeeze(1).cpu().numpy()/np.linalg.norm(classifier.encode_batch(waveform).squeeze(1).cpu().numpy())
+        embedding = load_classifier().encode_batch(waveform).squeeze(1).cpu().numpy()/np.linalg.norm(load_classifier().encode_batch(waveform).squeeze(1).cpu().numpy())
 
         # Initialize variables to find the recognized speaker
         min_distance = float('inf')
         recognized_speaker_id = None
 
-        with sqlite3.connect(DB_FILE) as conn:
-            cur = conn.cursor()
-            cur.execute("SELECT id, speaker_name, embedding FROM speakers")
-            rows = cur.fetchall()
-
-        known_speaker_ids = [row[1] for row in rows]
-        known_speakers = [np.array(json.loads(row[2])) for row in rows]
+        speakers = db.query(models.Speaker).all()
+        known_speaker_ids = [speaker.speaker_name for speaker in speakers]
+        known_speakers = [np.array(json.loads(speaker.embedding)) for speaker in speakers]
 
         # Compare the segment's embedding to each known speaker's embedding using cosine distance
         for i, speaker_embedding in enumerate(known_speakers):
@@ -704,7 +714,7 @@ async def asr(file: UploadFile):
                 "end_time": end_time,
             })
     
-    for segment in transcript['segments']   :
+    for segment in transcript['segments']:
         start = segment['start']
         text = segment['text'].strip()
         start = math.floor(float(start))
@@ -725,3 +735,8 @@ async def asr(file: UploadFile):
         })
 
     return aligned_output
+
+# Démarrage de l'application
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
