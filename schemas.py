170d1115a41d0cdb2dab7f66b8432e618f547806
Natha-daa <nathanaeldaawe@gmail.com>
2025-09-03 14:18:30 +0200

    Update schemas.py

--- schemas.py
+++ schemas.py
@@ -1,18 +1,21 @@
-# app/schemas.py
+# schemas.py
 from pydantic import BaseModel
+from datetime import datetime
 from typing import Optional
 
-class MediaCreate(BaseModel):
+class MediaBase(BaseModel):
     name: str
     description: Optional[str] = None
-    numberSpeaker: int = 1
     audioLink: Optional[str] = None
     videoLink: Optional[str] = None
+    coverLink: Optional[str] = None
     fileSize: Optional[str] = None
+    numberSpeaker: int
 
-class MediaResponse(MediaCreate):
+class MediaSchema(MediaBase):
     id: int
-    createdAt: Optional[str]
+    createdAt: str  # Définir comme chaîne pour la réponse JSON
 
     class Config:
-        from_attributes = True
+        orm_mode = True  # Permet l'utilisation avec SQLAlchemy
+        # Pydantic convertit automatiquement datetime en chaîne ISO avec orm_mode
